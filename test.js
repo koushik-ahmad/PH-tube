77c7ce2ffd94426681bb294c7b7bce8702950d97
koushik-ahmad <koushikahmad123@gmail.com>
2025-01-14 18:43:43 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+function getTimeString(time) {
+  // get hour and rest seconds
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  return `${hour} hour ${minute} minute ${remainingSecond} age`;
+}
+
+console.log(getTimeString(4320));
